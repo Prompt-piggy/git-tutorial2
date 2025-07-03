ee33ec272a9e4247f9f4d038da38f6b42502463b
Prompt-piggy <prachrattakarn@gmail.com>
2025-07-03 17:55:03 +0700

    Prompt

---
+++ myname.php
@@ -0,0 +1,22 @@
+<?php 
+   echo "T1";
+   echo "PRX";
+   echo "Sentine;";
+   echo "cgrs"; 
+   echo "T1";
+   echo "PRX";
+   echo "Sentine;";
+   echo "cgyugrs"; 
+   echo "T1";
+   echo "PRX";
+   echo "Sentine;";
+   echo "cgrs"; 
+   echo "T1";
+   echo "PRX";
+   echo "Sentine;";
+   echo "cgrs"; 
+   echo "T1";
+   echo "PRX";
+   echo "Sentine;";
+   echo "cgrs"; 
+?>
